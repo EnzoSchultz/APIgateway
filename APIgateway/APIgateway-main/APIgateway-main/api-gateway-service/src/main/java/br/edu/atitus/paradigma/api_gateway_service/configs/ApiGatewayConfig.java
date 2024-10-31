000160265138e74a0864a7ed4220830fbf591910
Enzo Schultz <135671484+EnzoSchultz@users.noreply.github.com>
2024-10-31 14:20:31 -0300

    Add files via upload

---
+++ APIgateway/APIgateway-main/APIgateway-main/api-gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/configs/ApiGatewayConfig.java
@@ -0,0 +1,28 @@
+package br.edu.atitus.paradigma.api_gateway_service.configs;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+	
+	@Bean
+	RouteLocator getGatewayRoute (RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(route -> route
+						.path("/cambio-service/**")
+						.filters(f -> f.addRequestHeader("Usuario", "Bernardo Sozo Fattini"))
+						.uri("lb://cambio-service"))
+				.route(route -> route
+						.path("/produto-service/**")
+						.filters(f -> f.addRequestHeader("Usuario", "Bernardo Sozo Fattini"))
+						.uri("lb://produto-service"))
+				.route(route -> route
+						.path("/saudacao-service/**")
+						.filters(f -> f.addRequestHeader("Usuario", "Bernardo Sozo Fattini"))
+						.uri("lb://saudacao-service"))
+				.build();
+	}
+}
